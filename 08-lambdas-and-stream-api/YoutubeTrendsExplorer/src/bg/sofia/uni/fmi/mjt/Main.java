7c2717f2f050688bde3a8e48210ffea96ef9b0a8
Ignatoff <mdignatov@uni-sofia.bg>
2020-01-09 22:24:02 +0200

    Lambdas and stream API

---
+++ 08-lambdas-and-stream-api/YoutubeTrendsExplorer/src/bg/sofia/uni/fmi/mjt/Main.java
@@ -0,0 +1,8 @@
+package bg.sofia.uni.fmi.mjt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ 08-lambdas-and-stream-api/YoutubeTrendsExplorer/src/bg/sofia/uni/fmi/mjt/youtube/YoutubeTrendsExplorer.java
@@ -0,0 +1,48 @@
+package bg.sofia.uni.fmi.mjt.youtube;
+
+import bg.sofia.uni.fmi.mjt.youtube.model.TrendingVideo;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.List;
+
+public class YoutubeTrendsExplorer {
+
+    /**
+     * Loads the dataset from the given {@code dataInput} stream.
+     */
+    public YoutubeTrendsExplorer(InputStream dataInput) {
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+
+    /**
+     * Returns all videos loaded from the dataset.
+     **/
+    public Collection<TrendingVideo> getTrendingVideos() {
+        return null;
+    }
+
+    public String findIdOfLeastLikedVideo() {
+
+    }
+
+    public String findIdOfMostLikedLeastDislikedVideo() {
+
+    }
+
+    public List<String> findDistinctTitlesOfTop3VideosByViews() {
+
+    }
+
+    public String findIdOfMostTaggedVideo() {
+
+    }
+
+    public String findTitleOfFirstVideoTrendingBefore100KViews() {
+
+    }
+
+    public String findIdOfMostTrendingVideo() {
+
+    }
+}
